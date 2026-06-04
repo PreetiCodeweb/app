84a646b6c8d6447fc2271d65a476f13f44f238ff
Preeti_code <147413752+PreetiCodeweb@users.noreply.github.com>
2026-06-04 20:05:04 +0530

    Initial Commit

---
+++ app/appApp.swift
@@ -0,0 +1,17 @@
+//
+//  appApp.swift
+//  app
+//
+//  Created by Preeti Sasmal on 04/06/26.
+//
+
+import SwiftUI
+
+@main
+struct appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
